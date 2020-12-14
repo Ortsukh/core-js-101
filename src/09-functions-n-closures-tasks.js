2a547d65ae12d001484996db6c037ec8c0c0b893
Ortsukh <54360787+Ortsukh@users.noreply.github.com>
2020-12-14 20:08:05 +0300

    feat: add solution

--- src/09-functions-n-closures-tasks.js
+++ src/09-functions-n-closures-tasks.js
@@ -86,7 +86,10 @@ function getPolynom() {
  *   memoizer() => the same random number  (next run, returns the previous cached result)
  */
 function memoize(func) {
-  return func;
+  const a = func();
+  return function () {
+    return a;
+  };
 }
 
 /**
@@ -148,8 +151,10 @@ function logger(/* func, logFunc */) {
  *   partialUsingArguments(fn, 'a','b','c')('d') => 'abcd'
  *   partialUsingArguments(fn, 'a','b','c','d')() => 'abcd'
  */
-function partialUsingArguments(/* fn, ...args1 */) {
-  throw new Error('Not implemented');
+function partialUsingArguments(fn, ...args1) {
+  return function (...args2) {
+    return fn(...args1, ...args2);
+  };
 }
 
 /**
@@ -169,8 +174,13 @@ function partialUsingArguments(/* fn, ...args1 */) {
  *   getId4() => 7
  *   getId10() => 11
  */
-function getIdGeneratorFunction(/* startFrom */) {
-  throw new Error('Not implemented');
+function getIdGeneratorFunction(startFrom) {
+  let el = startFrom - 1;
+  return function () {
+    // eslint-disable-next-line no-plusplus
+    el++;
+    return el;
+  };
 }
 
 module.exports = {
